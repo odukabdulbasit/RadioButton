becf939f80bb96fd5db239f4632883a4f538ad67
Abdulbasit Oduk <a.oduk@pegb.tech>
2023-08-10 14:05:56 +0300

    Added code logic to Main Activity Class

--- app/src/main/java/com/odukabdulbasit/radiobutton/MainActivity.kt
+++ app/src/main/java/com/odukabdulbasit/radiobutton/MainActivity.kt
@@ -2,10 +2,26 @@ package com.odukabdulbasit.radiobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+        val buttonShowSelection: Button = findViewById(R.id.buttonShowSelection)
+        val textViewSelection: TextView = findViewById(R.id.textViewSelection)
+
+        buttonShowSelection.setOnClickListener {
+            val selectedOptionId = radioGroup.checkedRadioButtonId
+            val selectedRadioButton: RadioButton = findViewById(selectedOptionId)
+
+            val selectedText = selectedRadioButton.text.toString()
+            textViewSelection.text = "Selected Option: $selectedText"
+        }
     }
 }
